c5624c76a1950e3ba19eece0719ed5257f661fe6
sanjeevlj <sd.sanjeev@gmail.com>
2025-01-19 23:04:36 +0530

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,59 @@
 import { useEffect, useState } from "react";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  const { user, signOut } = useAuthenticator();
+  const [devices, setDevices] = useState<Array<Schema["Device"]["type"]>>([]);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  useEffect(() => {
+    // Fetch devices for the current user
+    const subscription = client.models.Device.observeQuery({
+      username: user?.signInDetails?.loginId, // Filter devices by username
+    }).subscribe({
+      next: (data) => setDevices([...data.items]),
+    });
+
+    return () => subscription.unsubscribe(); // Clean up subscription on unmount
+  }, [user]);
+
+  function createDevice() {
+    const deviceID = window.prompt("Enter Device ID:");
+    const typeOfUser = window.prompt("Enter Type of User:");
+    const remarks = window.prompt("Enter Remarks (optional):");
+
+    if (deviceID && typeOfUser) {
+      client.models.Device.create({
+        username: user?.signInDetails?.loginId,
+        deviceID,
+        typeOfUser,
+        remarks,
+      });
+    }
   }
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+
+  function deleteDevice(id: string) {
+    client.models.Device.delete({ id });
   }
+
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}'s Devices</h1>
+      <button onClick={createDevice}>+ Add New Device</button>
       <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
+        {devices.map((device) => (
+          <li key={device.id}>
+            <strong>Device ID:</strong> {device.deviceID} <br />
+            <strong>Type of User:</strong> {device.typeOfUser} <br />
+            <strong>Remarks:</strong> {device.remarks || "N/A"} <br />
+            <button onClick={() => deleteDevice(device.id)}>Delete</button>
+          </li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign Out</button>
     </main>
   );
 }
